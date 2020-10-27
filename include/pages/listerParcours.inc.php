05ec04d189afa3970545fd87128da88d5ee84d58
taconet1 <62751602+taconet1@users.noreply.github.com>
2020-10-27 16:45:44 +0100

    Refactoring

--- include/pages/listerParcours.inc.php
+++ include/pages/listerParcours.inc.php
@@ -8,26 +8,27 @@ $parcoursManager = new ParcoursManager($pdo);
 
 $total=$parcoursManager->nombre();
 if ($total == 1) {?>
-  Actuellement <?php echo $total?> parcours est enregistré
+  Actuellement <?php echo $total?> parcours est enregistré<br><br>
 <?php }else{ ?>
-  Actuellement <?php echo $total?> parcours sont enregistrés
+  Actuellement <?php echo $total?> parcours sont enregistrés<br><br>
 <?php }?>
 
 <table>
-  <tr>
-    <td>Numéro</td>
-    <td>Nom ville</td>
-    <td>Nom Ville</td>
-    <td>Nombre de Km</td>
-  </tr>
-<?php
-$parcours=$parcoursManager->getAllParcours();
-foreach ($parcours as $par){ ?>
-		<tr><td><?php echo $par['par_num'];?>
-		</td><td><?php echo $par['ville1'];?>
-		</td><td><?php echo $par['ville2'];?>
-    </td><td><?php echo $par['par_km'];?>
-		</td></tr>
-	<?php }?>
-
+  <thead>
+    <th>Numéro</th>
+    <th>Nom ville</th>
+    <th>Nom Ville</th>
+    <th>Nombre de Km</th>
+  </thead>
+  <tbody>
+    <?php
+    $parcours=$parcoursManager->getAllParcours();
+    foreach ($parcours as $par){ ?>
+    		<tr><td><?php echo $par['par_num'];?>
+    		</td><td><?php echo $par['ville1'];?>
+    		</td><td><?php echo $par['ville2'];?>
+        </td><td><?php echo $par['par_km'];?>
+    		</td></tr>
+    	<?php }?>
+  </tbody>
 </table>
