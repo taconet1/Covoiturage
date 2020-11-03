edbd4a2643bf246ad79256f49f6d143b796402d0
taconet1 <62751602+taconet1@users.noreply.github.com>
2020-11-03 17:15:26 +0100

    Update listerPersonnes.inc.php

---
+++ include/pages/listerPersonnes.inc.php
@@ -0,0 +1,78 @@
+<?php if (empty($_GET["id"])): ?>
+  <h1>Liste des personnes enregistrées</h1>
+  Actuellement <?php echo $personneManager->getNombrePersonne(); ?> personnes enregistrées<br><br>
+
+  <?php $listePersonnes = $personneManager->getAllPersonne();?>
+    <table>
+      <thead>
+        <th>Numéro</th>
+        <th>Nom</th>
+        <th>Prénom</th>
+      </thead>
+
+      <tbody>
+      <?php foreach ($listePersonnes as $personne): $i=0;?>
+          <tr>
+            <td>
+              <a href="index.php?page=2&id=<?php echo $personne->getPerNum(); ?>" >
+                <?php echo $personne->getPerNum(); ?>
+              </a>
+            </td>
+            <td><?php echo $personne->getPerNom(); ?></td>
+            <td><?php echo $personne->getPerPrenom(); ?></td>
+          </tr>
+      <?php endforeach; ?>
+      </tbody>
+    </table>
+<?php endif; ?>
+
+
+<?php if (!empty($_GET["id"])):?>
+  <?php if (!empty($personneManager->getEtudiant($_GET["id"]))):
+    $personne=$personneManager->getEtudiant($_GET["id"]);?>
+
+  <h1>Détail sur l'étudiant <?php echo $personne->getPerNom(); ?></h1>
+  <table>
+    <thead>
+      <th>Prénom</th>
+      <th>Mail</th>
+      <th>Tel</th>
+      <th>Département</th>
+      <th>Ville</th>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td><?php echo $personne->getPerPrenom(); ?></td>
+        <td><?php echo $personne->getPerMail(); ?></td>
+        <td><?php echo $personne->getPerTel(); ?></td>
+        <td><?php echo $departementManager->getDep($personne->getDepNum()); ?></td>
+        <td><?php echo $departementManager->getDepVil($personne->getDepNum()); ?></td>
+      </tr>
+    </tbody>
+  </table>
+
+  <?php else:
+      $personne=$personneManager->getSalarie($_GET["id"]);?>
+    <h1>Détail sur le salarié <?php echo $personne->getPerNom(); ?></h1>
+    <table>
+      <thead>
+        <th>Prénom</th>
+        <th>Mail</th>
+        <th>Tel</th>
+        <th>Tel pro</th>
+        <th>Fonction</th>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td><?php echo $personne->getPerPrenom(); ?></td>
+          <td><?php echo $personne->getPerMail(); ?></td>
+          <td><?php echo $personne->getPerTel(); ?></td>
+          <td><?php echo $personne->getTelProf(); ?></td>
+          <td><?php echo $salarieManager->getSalFon($personne->getPerNum()); ?></td>
+        </tr>
+      </tbody>
+    </table>
+  <?php endif; ?>
+<?php endif; ?>
