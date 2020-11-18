2420d3b7b2a0e714361b4edbe0a4313415c9f483
doriancler <63239469+doriancler@users.noreply.github.com>
2020-11-18 08:48:05 +0100

    Update Connexion.inc.php
    se connecter interface

--- include/pages/Connexion.inc.php
+++ include/pages/Connexion.inc.php
@@ -1,2 +1,14 @@
 
 <h1>Pour vous connecter</h1>
+<form action="#" method="POST">
+  Nom d'utilisateur: <br>
+  <input type="text" name="nomUtilisateur"><br>
+  Mot de passe:<br>
+  <input type="password" name="mdp"><br>
+
+  <?php $nb1=rand(1,9); $nb2=rand(1,9);
+  echo "$nb1 + $nb2 = "
+  ?><br>
+  <input type="number" name="nombre"><br>
+  <input type="submit" value="Valider">
+</form>
