526d1c9aa1f6da13229f21f3996ca7f4494b0094
taconet1 <62751602+taconet1@users.noreply.github.com>
2020-12-08 22:35:42 +0100

    Empecher d'afficher Proposer et Chercher en changeant l'URL

--- include/pages/ChercherTrajet.inc.php
+++ include/pages/ChercherTrajet.inc.php
@@ -1,117 +1,126 @@
-<?php include_once("include/functions.inc.php"); ?>
+<?php
+include_once("include/functions.inc.php");
 
-<h1>Rechercher un trajet</h1>
+?>
 
-<?php if (empty($_POST["ville_depart"]) && empty($_POST["arrivee"])): ?>
-  <form id="recherche" action="#" method="post">
-    <label for="ville_depart">Ville de départ : </label>
-    <?php $recherche=$proposeManager->getListeVilleDepartDeLaRecherche();?>
-    <select name="ville_depart" onChange='javascript:document.getElementById("recherche").submit()';>
-        <option value="0">Choisissez</option>
-      <?php foreach ($recherche as $ville): ?>
-        <option value="<?php echo $ville->getVilNum(); ?>"><?php echo $ville->getVilNom(); ?></option>
-      <?php endforeach; ?>
-    </select>
-  </form>
-<?php endif; ?>
+<?php if (empty($_SESSION['nomUtilisateur'])) {
+  header('location: index.php');
+}else {?>
 
+  <h1>Rechercher un trajet</h1>
 
-<?php if (!empty($_POST["ville_depart"])): $_SESSION["ville_depart"]=$_POST["ville_depart"];?>
-  <form id="trajet" action="#" method="post">
-    <div class="gauche">
-      <p>Ville de départ : <?php echo $villeManager->getVil($_POST["ville_depart"]); ?></p><br>
-
-      <label for="date_depart">Date de départ : </label>
-      <input type="date" name="date_depart" value="<?php $dateDepart=date("Y-m-d"); echo $dateDepart; ?>"><br>
-
-      <label for="heure">A partir de : </label>
-      <select name="heure">
-        <?php for($i=0; $i < 24; $i++): ?>
-          <option value="<?php echo $i; ?>"><?php echo $i; ?>h</option>
-        <?php endfor; ?>
-      </select>
-    </div>
-
-    <div class="droite">
-      <label for="arrivee">Ville d'arrivée : </label>
-      <select name="arrivee">
-        <?php $villes=$proposeManager->getListeVilleArrivee($_POST["ville_depart"]); ?>
-        <?php foreach ($villes as $ville): ?>
+  <?php if (empty($_POST["ville_depart"]) && empty($_POST["arrivee"])): ?>
+    <form id="recherche" action="#" method="post">
+      <label for="ville_depart">Ville de départ : </label>
+      <?php $recherche=$proposeManager->getListeVilleDepartDeLaRecherche();?>
+      <select name="ville_depart" onChange='javascript:document.getElementById("recherche").submit()';>
+          <option value="0">Choisissez</option>
+        <?php foreach ($recherche as $ville): ?>
           <option value="<?php echo $ville->getVilNum(); ?>"><?php echo $ville->getVilNom(); ?></option>
         <?php endforeach; ?>
-      </select><br>
-
-      <label for="precision">Précision : </label>
-      <select name="precision">
-          <option value="0">Ce jour</option>
-        <?php for ($i=1;$i<=3;$i++) : ?>
-          <option value="<?php echo $i; ?>"><?php echo "+/- ".$i." jour(s)"; ?></option>
-        <?php endfor; ?>
       </select>
-    </div><br>
-
-    <input type="submit" value="Valider">
-  </form>
-<?php endif; ?>
-
-<?php if (!empty($_SESSION["ville_depart"]) && !empty($_POST["arrivee"])): ?>
-  <?php
-    $parNum = $proposeManager->getParNum($_SESSION["ville_depart"],$_POST["arrivee"]);
-    $dateDebut = date('Y-m-d', strtotime($_POST['date_depart']."-".$_POST['precision'].'days'));
-    $dateFin = date('Y-m-d', strtotime($_POST['date_depart']."+".$_POST['precision'].'days'));
-    $heureDepart = $_POST["heure"];
-    $sens = $proposeManager->getSensTrajet($_SESSION["ville_depart"]);
-    $infos = array('par_num' => $parNum,'dateDebut' => $dateDebut, 'dateFin' => $dateFin, 'heureDepart' => $heureDepart, 'pro_sens' => $sens);
-    $trajets = $proposeManager->getAllTrajetsRecherches($infos);
-  ?>
-
-  <?php if (empty($trajets)): ?>
-    <img src="image/erreur.png" alt="Erreur">
-    <span>Désolé, pas de trajet disponible !</span>
+    </form>
   <?php endif; ?>
 
-  <?php if (!empty($trajets)): ?>
-    <table>
-      <thead>
-        <th>Ville départ</th>
-        <th>Ville arrivée</th>
-        <th>Date départ</th>
-        <th>Heure départ</th>
-        <th>Nombre de place(s)</th>
-        <th>Nom du covoitureur</th>
-      </thead>
 
-      <tbody>
-        <?php foreach ($trajets as $trajet): ?>
-          <tr>
-            <td><?php echo $villeManager->getVil($_SESSION["ville_depart"]); ?></td>
-            <td><?php echo $villeManager->getVil($_POST["arrivee"]); ?></td>
-            <td>
-              <?php
-                $date=getUnitedKingdomDate($trajet->getProDate());
-                echo $date;
-              ?>
-            </td>
-            <td><?php echo $trajet->getProTime(); ?></td>
-            <td><?php echo $trajet->getProPlace(); ?></td>
-            <td>
-              <?php
-                $moyenne=$avisManager->getMoyenneAvis($parNum, $trajet->getPerNum());
-                $commentaire=$avisManager->getAvisCommentaire($parNum, $trajet->getPerNum());
-                if ($moyenne==null || $commentaire==false) {
-                  $moyenne="Aucune note";
-                  $commentaire="Aucun commentaire";
-                }
-              ?>
-              <a class="nomCovoit" title="<?php echo "Moyenne des avis : ".$moyenne."\nDernier avis : ".$commentaire; ?>">
-                <?php $personne=$personneManager->getPrenomNom($trajet->getPerNum());
-                      echo $personne[0]." ".$personne[1];
+  <?php if (!empty($_POST["ville_depart"])): $_SESSION["ville_depart"]=$_POST["ville_depart"];?>
+    <form id="trajet" action="#" method="post">
+      <div class="gauche">
+        <p>Ville de départ : <?php echo $villeManager->getVil($_POST["ville_depart"]); ?></p><br>
+
+        <label for="date_depart">Date de départ : </label>
+        <input type="date" name="date_depart" value="<?php $dateDepart=date("Y-m-d"); echo $dateDepart; ?>"><br>
+
+        <label for="heure">A partir de : </label>
+        <select name="heure">
+          <?php for($i=0; $i < 24; $i++): ?>
+            <option value="<?php echo $i; ?>"><?php echo $i; ?>h</option>
+          <?php endfor; ?>
+        </select>
+      </div>
+
+      <div class="droite">
+        <label for="arrivee">Ville d'arrivée : </label>
+        <select name="arrivee">
+          <?php $villes=$proposeManager->getListeVilleArrivee($_POST["ville_depart"]); ?>
+          <?php foreach ($villes as $ville): ?>
+            <option value="<?php echo $ville->getVilNum(); ?>"><?php echo $ville->getVilNom(); ?></option>
+          <?php endforeach; ?>
+        </select><br>
+
+        <label for="precision">Précision : </label>
+        <select name="precision">
+            <option value="0">Ce jour</option>
+          <?php for ($i=1;$i<=3;$i++) : ?>
+            <option value="<?php echo $i; ?>"><?php echo "+/- ".$i." jour(s)"; ?></option>
+          <?php endfor; ?>
+        </select>
+      </div><br>
+
+      <input type="submit" value="Valider">
+    </form>
+  <?php endif; ?>
+
+  <?php if (!empty($_SESSION["ville_depart"]) && !empty($_POST["arrivee"])): ?>
+    <?php
+      $parNum = $proposeManager->getParNum($_SESSION["ville_depart"],$_POST["arrivee"]);
+      $dateDebut = date('Y-m-d', strtotime($_POST['date_depart']."-".$_POST['precision'].'days'));
+      $dateFin = date('Y-m-d', strtotime($_POST['date_depart']."+".$_POST['precision'].'days'));
+      $heureDepart = $_POST["heure"];
+      $sens = $proposeManager->getSensTrajet($_SESSION["ville_depart"]);
+      $infos = array('par_num' => $parNum,'dateDebut' => $dateDebut, 'dateFin' => $dateFin, 'heureDepart' => $heureDepart, 'pro_sens' => $sens);
+      $trajets = $proposeManager->getAllTrajetsRecherches($infos);
+    ?>
+
+    <?php if (empty($trajets)): ?>
+      <img src="image/erreur.png" alt="Erreur">
+      <span>Désolé, pas de trajet disponible !</span>
+    <?php endif; ?>
+
+    <?php if (!empty($trajets)): ?>
+      <table>
+        <thead>
+          <th>Ville départ</th>
+          <th>Ville arrivée</th>
+          <th>Date départ</th>
+          <th>Heure départ</th>
+          <th>Nombre de place(s)</th>
+          <th>Nom du covoitureur</th>
+        </thead>
+
+        <tbody>
+          <?php foreach ($trajets as $trajet): ?>
+            <tr>
+              <td><?php echo $villeManager->getVil($_SESSION["ville_depart"]); ?></td>
+              <td><?php echo $villeManager->getVil($_POST["arrivee"]); ?></td>
+              <td>
+                <?php
+                  $date=getUnitedKingdomDate($trajet->getProDate());
+                  echo $date;
                 ?>
-              </a>
-            </td>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
+              </td>
+              <td><?php echo $trajet->getProTime(); ?></td>
+              <td><?php echo $trajet->getProPlace(); ?></td>
+              <td>
+                <?php
+                  $moyenne=$avisManager->getMoyenneAvis($parNum, $trajet->getPerNum());
+                  $commentaire=$avisManager->getAvisCommentaire($parNum, $trajet->getPerNum());
+                  if ($moyenne==null || $commentaire==false) {
+                    $moyenne="Aucune note";
+                    $commentaire="Aucun commentaire";
+                  }
+                ?>
+                <a class="nomCovoit" title="<?php echo "Moyenne des avis : ".$moyenne."\nDernier avis : ".$commentaire; ?>">
+                  <?php $personne=$personneManager->getPrenomNom($trajet->getPerNum());
+                        echo $personne[0]." ".$personne[1];
+                  ?>
+                </a>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    <?php endif; ?>
   <?php endif; ?>
-<?php endif; ?>
+
+<?php  } ?>
--- include/pages/ProposerTrajet.inc.php
+++ include/pages/ProposerTrajet.inc.php
@@ -1,64 +1,69 @@
-<h1>Proposer un trajet</h1>
+<?php if (empty($_SESSION['nomUtilisateur'])) {
+    header('location: index.php');
+} else {?>
+  <h1>Proposer un trajet</h1>
 
-<?php if (empty($_POST["depart"]) && empty($_POST["arrivee"])): ?>
-  <form id="proposer" action="#" method="post">
-    <label for="depart">Ville de départ : </label><br><br>
-    <select name="depart" onChange='javascript:document.getElementById("proposer").submit()'>
-        <option value="0">Choisissez</option>
-      <?php $parcours=$proposeManager->getListeVilleDepart();
-            foreach ($parcours as $ville): ?>
-        <option value="<?php echo $ville->getVilNum(); ?>"><?php echo $ville->getVilNom(); ?></option>
-      <?php endforeach; ?>
-    </select>
-  </form>
-
-<?php endif; ?>
-
-<?php if (!empty($_POST["depart"])): ?>
-  <form id="trajet" action="#" method="post">
-    <div class="gauche">
-      <p id="villeDepart">Ville de départ : <?php echo $villeManager->getVil($_POST["depart"]); ?></p><br>
-
-      <?php date_default_timezone_set("Europe/Amsterdam");?>
-      <label for="pro_date">Date de départ : </label>
-      <input type="date" name="pro_date" value="<?php $dateDepart=date("Y-m-d"); echo $dateDepart; ?>"><br>
-
-      <label for="pro_place">Nombre de places : </label>
-      <input type="number" name="pro_place" required>
-    </div>
-
-    <div class="droite">
-      <label for="arrivee">Ville d'arrivée : </label>
-      <select name="arrivee">
-        <?php $listeVille=$proposeManager->getListeVilleArrivee($_POST["depart"]);
-              foreach ($listeVille as $ville): ?>
+  <?php if (empty($_POST["depart"]) && empty($_POST["arrivee"])): ?>
+    <form id="proposer" action="#" method="post">
+      <label for="depart">Ville de départ : </label><br><br>
+      <select name="depart" onChange='javascript:document.getElementById("proposer").submit()'>
+          <option value="0">Choisissez</option>
+        <?php $parcours=$proposeManager->getListeVilleDepart();
+              foreach ($parcours as $ville): ?>
           <option value="<?php echo $ville->getVilNum(); ?>"><?php echo $ville->getVilNom(); ?></option>
         <?php endforeach; ?>
-      </select><br>
+      </select>
+    </form>
 
-      <label for="pro_time">Heure de départ : </label>
-      <input type="time" name="pro_time" value="<?php $horaireDepart=date("H:i:s"); echo $horaireDepart; ?>">
-    </div><br>
+  <?php endif; ?>
 
-    <input type="submit" value="Valider">
-  </form>
-  <?php $_SESSION['depart']=$_POST["depart"]; ?>
-<?php endif; ?>
+  <?php if (!empty($_POST["depart"])): ?>
+    <form id="trajet" action="#" method="post">
+      <div class="gauche">
+        <p id="villeDepart">Ville de départ : <?php echo $villeManager->getVil($_POST["depart"]); ?></p><br>
 
-<?php if (!empty($_SESSION['depart']) && !empty($_POST["arrivee"])):
-  $trajet=new Propose($_POST);
+        <?php date_default_timezone_set("Europe/Amsterdam");?>
+        <label for="pro_date">Date de départ : </label>
+        <input type="date" name="pro_date" value="<?php $dateDepart=date("Y-m-d"); echo $dateDepart; ?>"><br>
 
-  $trajet->setParNum($proposeManager->getParNum($_SESSION['depart'], $_POST["arrivee"]));
-  $trajet->setPerNum($personneManager->getPerNum($_SESSION["nomUtilisateur"]));
-  $trajet->setProSens($proposeManager->getSensTrajet($_SESSION['depart']));
+        <label for="pro_place">Nombre de places : </label>
+        <input type="number" name="pro_place" required>
+      </div>
 
-  if ($proposeManager->existe($trajet)==false) {
-    $estAjoute = $proposeManager->ajouter($trajet);
-    if ($estAjoute==true) {?>
-      <img src="image/valid.png" alt="Valid"> Votre proposition a été prise en compte
-    <?php }
-  } else{?>
-    <img src="image/erreur.png" alt="Erreur"> Votre proposition a déjà été enregistré
-  <?php }?>
+      <div class="droite">
+        <label for="arrivee">Ville d'arrivée : </label>
+        <select name="arrivee">
+          <?php $listeVille=$proposeManager->getListeVilleArrivee($_POST["depart"]);
+                foreach ($listeVille as $ville): ?>
+            <option value="<?php echo $ville->getVilNum(); ?>"><?php echo $ville->getVilNom(); ?></option>
+          <?php endforeach; ?>
+        </select><br>
 
-<?php endif; ?>
+        <label for="pro_time">Heure de départ : </label>
+        <input type="time" name="pro_time" value="<?php $horaireDepart=date("H:i:s"); echo $horaireDepart; ?>">
+      </div><br>
+
+      <input type="submit" value="Valider">
+    </form>
+    <?php $_SESSION['depart']=$_POST["depart"]; ?>
+  <?php endif; ?>
+
+  <?php if (!empty($_SESSION['depart']) && !empty($_POST["arrivee"])):
+    $trajet=new Propose($_POST);
+
+    $trajet->setParNum($proposeManager->getParNum($_SESSION['depart'], $_POST["arrivee"]));
+    $trajet->setPerNum($personneManager->getPerNum($_SESSION["nomUtilisateur"]));
+    $trajet->setProSens($proposeManager->getSensTrajet($_SESSION['depart']));
+
+    if ($proposeManager->existe($trajet)==false) {
+      $estAjoute = $proposeManager->ajouter($trajet);
+      if ($estAjoute==true) {?>
+        <img src="image/valid.png" alt="Valid"> Votre proposition a été prise en compte
+      <?php }
+    } else{?>
+      <img src="image/erreur.png" alt="Erreur"> Votre proposition a déjà été enregistré
+    <?php }?>
+
+  <?php endif; ?>
+
+<?php }?>
